4eaedb54071ee2d730ba6b1ad67a95e196c79e4e
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-03-08 22:15:53 +0100

    mounted AlbumCard component inside AlbumList instead of App

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import { AlbumCard } from 'components/AlbumCard'; // Ask Daniel on thursday why this has to be above the Header
+import { AlbumList } from 'components/AlbumList'; // Ask Daniel on thursday why this has to be above the Header
 import { Header } from './components/Header';
 
 import data from './data.json';
@@ -11,7 +11,7 @@ export const App = () => {
   return (
     <main className="wrapper">
       <Header />
-      <AlbumCard />
+      <AlbumList newReleases={data} />
     </main>
   );
 }
---
+++ code/src/components/AlbumList.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import { AlbumCard } from 'components/AlbumCard'; // Ask Daniel on thursday why this has to be above the Header
+
+export const AlbumList = () => {
+  console.log('the album list is here')
+  return (
+    <AlbumCard />
+  );
+}
