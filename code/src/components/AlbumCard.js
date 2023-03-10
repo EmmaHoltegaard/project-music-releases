e3e02a317ace21e0963e02fb59450f95d9dd0379
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-03-10 07:42:51 +0100

    unknown error, causes blank page except for background color. Abaondoning testing branch

--- code/src/components/AlbumCard.js
+++ code/src/components/AlbumCard.js
@@ -5,21 +5,17 @@ import { Icons } from './Icons';
 
 export const AlbumCard = (props) => {
   return (
-    <article>
+    <article className="album-card-wrapper">
       <div className="image-wrapper">
-        <a target="_blank" href={props.albumDetails.external_urls.spotify} rel="noreferrer"><img className="album-cover" src={props.albumDetails.images[1].url} alt="album cover" /></a>
+        <img className="album-cover" src={props.albumDetails.images[1].url} alt="album cover" />
         <Icons />
       </div>
-      <a target="_blank" href={props.albumDetails.external_urls.spotify} rel="noreferrer">
-        <h2 className="album-name">{props.albumDetails.name}
-        </h2>
-      </a>
-      <h3 className="artist-name">
-        {props.albumDetails.artists.map((artist) => {
-          return (
-            <AlbumArtist key={artist.id} artistDetails={artist} />
-          )
-        })}
+      <h2 className="album-name">{props.albumDetails.name}</h2>
+      <h3 className="artist-name">{props.albumDetails.artists.map((artist) => {
+        return (
+          <AlbumArtist key={artist.id} artistDetails={artist} />
+        )
+      })}
       </h3>
     </article>
   )
